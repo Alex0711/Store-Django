3fca7aec3fbf32137a94d87934f61247a25cf12f
Alex0711 <alex.senger@hotmail.com>
2022-03-07 06:29:29 -0300

    agregue archivos para heroku

--- store/settings.py
+++ store/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenose.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'store.urls'
